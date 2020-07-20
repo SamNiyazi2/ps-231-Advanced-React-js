0df2f09a506b5046bf269a7aea74d83078700ce8
Sam Niyazi <sam2003@niyazi.com>
2020-07-20 14:50:09 -0500

    Modification were already made

---
+++ ssn_run_server.cmd
@@ -0,0 +1,11 @@
+
+
+Rem 07/20/2020 12:51 pm - SSN - [20200720-0738] - [005] - M03 - Full-stack JavaScript with React.js
+
+cd %~dp0
+
+rem start "ps-advanced-react web server" node lib/server.js
+start "ps-advanced-react web server" yarn dev
+
+start "ps-advanced-react web page" ssn_run.cmd
+
