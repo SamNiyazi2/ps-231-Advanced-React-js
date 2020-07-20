4fe230120178e8992755fd6fb93a8f174c5508fb
Sam Niyazi <sam2003@niyazi.com>
2020-07-20 18:48:46 -0500

    Completed M03-P03

--- ssn_run_server.cmd
+++ ssn_run_server.cmd
@@ -2,10 +2,17 @@
 
 Rem 07/20/2020 12:51 pm - SSN - [20200720-0738] - [005] - M03 - Full-stack JavaScript with React.js
 
+
+set custom_site_port=3114
+
+call C:\sams\ps\KillProcess\KillProcess_Using_PortNo.cmd %site_port%
+
+
 cd %~dp0
 
 rem start "ps-advanced-react web server" node lib/server.js
-start "ps-advanced-react web server" yarn dev
+start "ps-advanced-react web server - yarn webpack" yarn webpack
+start "ps-advanced-react web server - yarn dev" yarn dev
 
 start "ps-advanced-react web page" ssn_run.cmd
 
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+
+// 07/20/2020 04:34 pm - SSN - [20200720-1620] - [002] - M03 - Configuring a full-stack JavaScript Environment: Client side
+
+const path = require('path');
+
+const config = {
+
+    entry: [
+        'babel-polyfill',
+        './lib/components/index.js'
+    ]
+        ,
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+
+    }
+    ,
+    module: {
+        rules: [
+            { test: /\.js|jsx$/, use: 'babel-loader',
+            exclude: /node_modules/
+         }
+        ]
+    }
+};
+
+
+module.exports = config;
+
