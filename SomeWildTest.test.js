5f00b54b4765bdae4a9830fa7e560bbde9ab5616
Sam Niyazi <sam2003@niyazi.com>
2020-07-21 13:03:24 -0500

    Completed M03-P09

---
+++ SomeWildTest.test.js
@@ -0,0 +1,16 @@
+
+// 07/21/2020 12:50 pm - SSN - [20200721-1205] - [002] - M03-P09 - Jest snapshot testing
+
+
+
+describe ('Test in the wild', () => {
+
+    it ( 'should show up regardless where is at', ()=> {
+
+        let value1 = '101';
+
+        expect( value1).toBe('101');
+    })
+
+});
+
